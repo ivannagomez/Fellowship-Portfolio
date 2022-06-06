3cdf26d2b526f99bdf834ee1e6a2222a7fc2019a
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-06-06 19:09:18 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="Ivannas Portfolio", url=os.getenv("URL"))
+
+@app.route("/about")
+def about_us():
+    return render_template("about.html")
+  
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
