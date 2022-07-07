36a1db17689341f03e665af4a0ebdfc5c9a4ecd0
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-07-07 04:30:34 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,18 @@ mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
 
 print(mydb)
 
+# Timeline
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.now)
 
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 #Landing page routing
 @app.route("/")
@@ -36,7 +47,23 @@ def hobbies():
 def timeline():
     return render_template("timeline.html")
   
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post(): 
+    return{
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
 
 if __name__ == "__main__":
     app.debug = True
