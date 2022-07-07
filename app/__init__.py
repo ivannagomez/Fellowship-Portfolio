8c9c768e00b6f090a2db928dde8f4ff03b34c52e
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-07-07 05:09:48 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -47,23 +47,7 @@ def hobbies():
 def timeline():
     return render_template("timeline.html")
   
-@app.route('/api/timeline_post', methods=['POST'])
-def post_timeline_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
-    return model_to_dict(timeline_post)
-
-@app.route('/api/timeline_post', methods=['GET'])
-def get_timeline_post(): 
-    return{
-        'timeline_posts': [
-            model_to_dict(p)
-            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-        ]
-    }
 
 if __name__ == "__main__":
     app.debug = True
