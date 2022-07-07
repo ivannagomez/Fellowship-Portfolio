f4fe07b0c7c09152c09db348d88d427ff19de1e3
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-07-07 02:30:58 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,19 @@ import os
 from flask import Flask, render_template, request, json
 from dotenv import load_dotenv
 import json
+from peewee import *
+from datetime import datetime
 
 load_dotenv()
 app = Flask(__name__)
 
+# connect to MySQL database
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                     user=os.getenv("MYSQL_USER"),
+                     password=os.getenv("MYSQL_PASSWORD"),
+                     port=3306)
+
+print(mydb)
 
 @app.route("/")
 def index():
