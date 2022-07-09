c1db1ddfa1119c03a35347b5610c5fdd540777a1
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-07-09 02:07:08 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd ~/Fellowship-Portfolio
 git fetch
 git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -ds flask-portfolio 'flask run --host=0.0.0.0;'
+systemctl restart myportfolio
