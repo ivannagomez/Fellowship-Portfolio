32989c4eb0bdcd5babbc19714f9f35d6576586f6
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-07-12 16:50:06 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
                      user=os.getenv("MYSQL_USER"),
                      password=os.getenv("MYSQL_PASSWORD"),
+                     host=os.getenv("MYSQL_HOST"),
                      port=3306)
 
 print(mydb)
