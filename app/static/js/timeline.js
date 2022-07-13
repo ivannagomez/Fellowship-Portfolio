969daac31c271b37d6257a4fdf233890f71fe308
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2022-07-13 16:29:37 -0400

    Update timeline.js

--- app/static/js/timeline.js
+++ app/static/js/timeline.js
@@ -15,9 +15,9 @@ const getPosts = () => {
 const post2html = (post) =>{
     return `
         <h5 style="line-height:0.8">${post['name']}</h5>
-        <li style="line-height:1.9">Content: ${post['content']}</il>
+        <li style="line-height:2.6">Content: ${post['content']}</il>
         <p style="line-height:0.6">${post['email']}</p>
-        <p style="line-height:0.6">${post['created_at']}</p>
+        <p style="line-height:0.4">${post['created_at']}</p>
         <p style="line-height:1.6"><br></p>
         `
 }
